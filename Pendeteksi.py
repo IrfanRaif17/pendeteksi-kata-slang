a3bf2eb624b3a069ec6bc60dd165977ba2825855
arifian853 <2001020056@student.umrah.ac.id>
2023-07-06 16:15:19 +0700

    first streamlit

--- Pendeteksi.py
+++ Pendeteksi.py
@@ -229,8 +229,9 @@ def main():
                     with open(file_path, 'rb') as file:
                         contents = file.read()
                         base64_encoded = base64.b64encode(contents).decode()
-                        href = f'<a href="data:text/{file_type};base64,{base64_encoded}" download="{file_path}">Download {file_type}</a>'
-                    return href
+                        href = f"data:text/{file_type};base64,{base64_encoded}"
+                        button = f'<a href="{href}" download="{file_path}"><button>Download {file_type}</button></a>'
+                    return button
 
                 # Export to text file
                 if st.button("Export ke TXT"):
